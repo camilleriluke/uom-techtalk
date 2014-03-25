362b48ca1112a2f04d95f73807e92047c7f94897
camilleriluke <mail@camilleriluke.com>
2014-03-25 12:36:11 +0100

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('webApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute']).config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
---
+++ scripts/controllers/main.js
@@ -0,0 +1,3 @@
+'use strict';
+angular.module('webApp').controller('MainCtrl', function($scope) {
+});
